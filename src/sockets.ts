214de4c773122e560c21ecc167630f7982ca4bf4
overcomer79 <pierpaolo.monaco@gmail.com>
2024-07-07 21:07:30 -0400

    add typing activity

--- src/sockets.ts
+++ src/sockets.ts
@@ -10,8 +10,27 @@ export const createIo = (server: http.Server) => {
     io.on('connection', socket => {
         console.log(`User ${socket.id} connected`)
 
+        // Upon Connection - only to the user
+        socket.emit('message','Welcome to Game Land!')
+        
+        // Upon Connection - to all other users
+        socket.broadcast.emit('message', `${socket.id.substring(0,5)} si e' connesso!`)
+
+        // Listening for a message event
         socket.on('message', data => {
             io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
+        })
+
+        // When User Disconnect = to all other users
+        socket.on('disconnect', () => {
+            console.log(`User ${socket.id} disconnected`)
+            socket.broadcast.emit('message', `${socket.id.substring(0,5)} si ha abbandonato...`)
+        })
+
+        // Listen for activity
+        socket.on('activity', (name) => {
+            console.log('activity', name)
+            socket.broadcast.emit('activity', name)
         })
     })
     return io
