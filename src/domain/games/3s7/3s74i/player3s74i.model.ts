17f488aee94e9f02c64ecdc08b39c7a93160dc5f
overcomer79 <pierpaolo.monaco@gmail.com>
2024-09-01 18:21:02 -0400

    Add positionin in the constructor

--- src/domain/games/3s7/3s74i/player3s74i.model.ts
+++ src/domain/games/3s7/3s74i/player3s74i.model.ts
@@ -1,18 +1,20 @@
-import { Player3s7 } from "./../player3s7.model";
+import { Player3s7 } from "./../player3s7.model"
+import { IPlayer3s74i } from './interfaces/IPlayer3s74i'
 
 export enum Position3s74i {
-    Nord,
-    Sud,
-    ovest,
-    Est
+    Nord = 2,
+    Sud = 0,
+    Ovest = 3,
+    Est = 1
 }
 
-export class Player3s74i extends Player3s7 {
+export class Player3s74i extends Player3s7 implements IPlayer3s74i {
 
     position: Position3s74i
 
-    constructor(username: string) {
+    constructor(username: string, position?: Position3s74i) {
         super(username)
-        
+        if (position !== undefined)
+            this.position = position
     }
 }
